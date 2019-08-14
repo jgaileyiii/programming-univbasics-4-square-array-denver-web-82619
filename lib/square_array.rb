18ea9bea4820484331c8190047e7f16b09e41f0b
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-14 17:51:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
-def square_array(array)
-  new_array = []
-  while new_array[] do 
-  array.each{|a| new_array<<(a*a)}
-  puts new_array
-end
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
 end
