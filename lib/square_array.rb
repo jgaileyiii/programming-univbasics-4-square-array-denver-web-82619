54b40321ff7a741cf291a066f660f8a2fd9d6dd4
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-14 18:07:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-
+numbers = []
  new_array = []
 
- while array.each do 
-  new_array << array ** 2
+ while numbers = 1,2,3 do 
+  new_array << numbers ** 2
  end
  return new_array
 end
